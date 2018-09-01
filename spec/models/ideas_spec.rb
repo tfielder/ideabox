33439b971ec1fcc4e38f65f496bb60d56d17677c
Tim Fielder <fiel1139@gmail.com>
2018-09-01 17:16:25 -0600

    created schema.

--- spec/models/ideas_spec.rb
+++ spec/models/ideas_spec.rb
@@ -4,7 +4,7 @@ describe Idea, type: :model do
   describe 'validations' do
     it {should validate_presence_of :title}
     it {should validate_presence_of :image}
-    it 
+    it {should validate_presence_of :note}
   end
   describe 'relationships' do
     it {should respond_to :category}
