30ed9154615a3bd44855060fe2a260343f9501e3
Tim Fielder <fiel1139@gmail.com>
2018-09-02 15:15:08 -0600

    adds feature test for category.

---
+++ spec/features/user_sees_all_categories_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'user sees all categories' do
+  describe 'they visit /categories' do
+    it 'displays all categories' do
+      cat1 = Category.create(topic: "Social")
+      cat2 = Category.create(topic: "Technology")
+
+      visit '/categories'
+
+      expect(page).to have_content("#{cat1.topic}")
+      expect(page).to have_content("#{cat2.topic}")
+
+    end
+  end
+end
