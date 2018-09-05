a087d6e305c4db0b27ecbc4322edbc440e4289d2
Tim Fielder <fiel1139@gmail.com>
2018-09-05 14:12:34 -0600

    user roles test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,22 @@ describe User, type: :model do
   describe 'relationships' do
     it {should have_many :ideas}
   end
+  describe "roles" do
+    it "can be created as an admin" do
+      user = User.create(username: "penelope",
+                         password: "sosecretboom",
+                         role: 1)
+
+      expect(user.role).to eq("admin")
+      expect(user.admin?).to be_truthy
+    end
+    it "can be created as a default user" do
+      user = User.create(username: "sunny",
+                         password: "secret",
+                         role: 0)
+
+      expect(user.role).to eq("default")
+      expect(user.default?).to be_truthy
+    end
+  end
 end
