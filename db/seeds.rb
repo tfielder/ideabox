30260b053c150c3f804d45aedc59bf4bd98e44c1
Tim Fielder <fiel1139@gmail.com>
2018-09-05 13:56:56 -0600

    seeds for categories.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cat01 = Category.create(topic: "Fun")
+cat02 = Category.create(topic: "Social")
+cat03 = Category.create(topic: "Sports")
+cat04 = Category.create(topic: "Food")
+cat05 = Category.create(topic: "Lunch")
+cat06 = Category.create(topic: "Vacations")
+cat07 = Category.create(topic: "Work")
+cat08 = Category.create(topic: "School")
+cat09 = Category.create(topic: "Summer")
+cat10 = Category.create(topic: "Chances")
