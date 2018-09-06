eab1c2410f3faae698d13b7fd2e151102ba5642e
Tim Fielder <fiel1139@gmail.com>
2018-09-05 22:34:06 -0600

    added description to images.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906041356) do
+ActiveRecord::Schema.define(version: 20180906043114) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20180906041356) do
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
