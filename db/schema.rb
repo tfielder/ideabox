bb0ab9adfb75558c3836b403d311a17f1e84b10a
Tim Fielder <fiel1139@gmail.com>
2018-09-05 22:14:26 -0600

    creates migration for ideaimages.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180906040515) do
+ActiveRecord::Schema.define(version: 20180906041356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20180906040515) do
     t.string "topic"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "idea_images", force: :cascade do |t|
+    t.bigint "idea_id"
+    t.bigint "image_id"
+    t.index ["idea_id"], name: "index_idea_images_on_idea_id"
+    t.index ["image_id"], name: "index_idea_images_on_image_id"
   end
 
   create_table "ideas", force: :cascade do |t|
@@ -45,6 +52,8 @@ ActiveRecord::Schema.define(version: 20180906040515) do
     t.integer "role", default: 0
   end
 
+  add_foreign_key "idea_images", "ideas"
+  add_foreign_key "idea_images", "images"
   add_foreign_key "ideas", "categories"
   add_foreign_key "ideas", "users"
 end
