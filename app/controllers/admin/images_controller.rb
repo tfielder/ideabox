5e5a270fafde73a5fed7293eab9a50ae739cd5ab
Tim Fielder <fiel1139@gmail.com>
2018-09-05 21:58:36 -0600

    sets up images controller under admin.

---
+++ app/controllers/admin/images_controller.rb
@@ -0,0 +1,40 @@
+class Admin::ImagesController < Admin::BaseController
+  before_action :set_image, only: [:edit, :update]
+
+  def index
+    @images = Image.all
+  end
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    image = Image.new(image_params)
+    image.save
+    redirect_to admin_images_path
+  end
+
+  def edit
+
+  end
+
+  def update
+    @image.update(image_params)
+    redirect_to admin_images_path
+  end
+
+  def destroy
+    @image = Image.destroy(params[:id])
+    redirect_to admin_images_path
+  end
+
+  private
+    def image_params
+      params.require(:image).permit(:url)
+    end
+
+    def set_image
+      @image = Image.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
 
   namespace :admin do
     resources :categories, only: [:index, :new, :create, :destroy, :edit, :update]
+    resources :images, only: [:index, :new, :create, :destroy, :edit, :update]
   end
 end
